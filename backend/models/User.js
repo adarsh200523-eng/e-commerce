465bb75c0edfeb86a7420e2bb08984b027aaef0e
Adarsh Raj <adarsh200523@gmail.com>
2026-08-28 16:20:46 +0530

    initial commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,4 +7,4 @@ const userSchema = new mongoose.Schema({
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema); 
