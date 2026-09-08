b221cf2e4c7af6334e8726351bbe90bf1877d86a
Adarsh Raj <adarsh200523@gmail.com>
2026-09-08 21:06:07 +0530

    initial commit

--- frontend/src/redux/cartSlice.js
+++ frontend/src/redux/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
+  cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [], 
 };
 
 const cartSlice = createSlice({
